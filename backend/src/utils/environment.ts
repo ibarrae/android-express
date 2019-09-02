4f0720cdd940d16d035d124ccd531340a096e906
Esteban Ibarra <eibarra1992@hotmail.com>
2019-09-02 17:13:02 -0500

    Refactor environment.ts

--- backend/src/utils/environment.ts
+++ backend/src/utils/environment.ts
@@ -2,13 +2,28 @@ import dotenv from "dotenv";
 
 const result = dotenv.config();
 
-if (!result.parsed) {
-  throw new Error("Failed loading .env");
-}
+const { jwtSecret, dbHost, dbUser, dbPassword, dbName } = (() => {
+  if (process.env.NODE_ENV === "test") {
+    return {
+      jwtSecret: "",
+      dbHost: "",
+      dbName: "",
+      dbPassword: "",
+      dbUser: ""
+    };
+  }
+  if (!result.parsed) {
+    throw new Error("Failed loading .env");
+  }
 
-const { JWT_SECRET, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = result.parsed;
-export const jwtSecret = JWT_SECRET;
-export const dbHost = DB_HOST;
-export const dbUser = DB_USER;
-export const dbPassword = DB_PASSWORD;
-export const dbName = DB_NAME;
+  const { JWT_SECRET, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = result.parsed;
+  return {
+    jwtSecret: JWT_SECRET,
+    dbHost: DB_HOST,
+    dbUser: DB_USER,
+    dbPassword: DB_PASSWORD,
+    dbName: DB_NAME
+  };
+})();
+
+export { jwtSecret, dbHost, dbUser, dbPassword, dbName };
